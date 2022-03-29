2d178aff16ad4a175b62d4a60f3033a1af02c113
Shiro Amurha <46139101+shiroamurha@users.noreply.github.com>
2022-03-29 18:19:27 -0300

    next att will switch from ugg to riot api

--- profileAnalizer.py
+++ profileAnalizer.py
@@ -1,51 +1,65 @@
 from playwright.sync_api import sync_playwright
-# from bs4 import BeautifulSoup as bsoup
+from bs4 import BeautifulSoup as bsoup
+import requests
 
 
 
 # browser and page init
-browser = sync_playwright().start().webkit.launch()
-page = browser.new_page()
+def driver_webscrapping():
+	browser = sync_playwright().start().webkit.launch()
+	page = browser.new_page()
 
-# go to url
-page.goto("https://u.gg/lol/profile/br1/s%20h%20iï%20r%20o/overview") #https://u.gg/lol/profile/br1/s%20h%20iï%20r%20o/overview
+	# go to url
+	page.goto("https://u.gg/lol/profile/br1/elinkgas13/overview") #https://u.gg/lol/profile/br1/s%20h%20iï%20r%20o/overview
 
-# clicking on update button for (obviously) uptading the infos
-page.locator(
-    "button[class='update-button']"
-).click()
+	# clicking on update button for (obviously) uptading the infos
+	page.locator(
+		"button[class='update-button']"
+	).click()
 
-summonerNames = list()
+	summonerNames = list()
 
-# select the div with the names in three last matches 
-for matchCard_nthIterator in range(0, 3):
-	summoner_names_selector = page.locator(
-		f"div[class='match-history_match-card'] >> nth={matchCard_nthIterator}"
-  
-			).locator("div[class='group-four']"
-            	)
+	# select the div with the names in three last matches 
+	for matchCard_nthIterator in range(0, 3):
+		summoner_names_selector = page.locator(
+			f"div[class='match-history_match-card'] >> nth={matchCard_nthIterator}"
 	
-	# select team div of summoner names inside the match div
-	for teamList_nthIterator in range(0,2):
+				).locator("div[class='group-four']"
+					)
 		
-		# for every summoner name in the team, summonerNames recieve each name 	
-		for summonerName_nthIterator in range(0, 5):
+		# select team div of summoner names inside the match div
+		for teamList_nthIterator in range(0,2):
+			
+			# for every summoner name in the team, summonerNames recieve each name 	
+			for summonerName_nthIterator in range(0, 5):
 
-			summonerNames.append(summoner_names_selector.locator(
-				f"div[class='team-list'] >> nth={teamList_nthIterator}"
-    
-					# name extracted from <a title="summoner name here">
-					).locator(f"a >> nth={summonerName_nthIterator}").get_attribute(name='title'))
+				summonerNames.append(summoner_names_selector.locator(
+					f"div[class='team-list'] >> nth={teamList_nthIterator}"
+		
+						# name extracted from <a title="summoner name here">
+						).locator(f"a >> nth={summonerName_nthIterator}").get_attribute(name='title'))
+	browser.close()
 
+	open('tome.txt', 'w', encoding='utf-8').write(f'{summonerNames}')
+
+	print(len(summonerNames))
 
 #########################################
 # 
-open(
-    'tome.txt', 
-    'w', 
-    encoding='utf-8').write(
-        f'{summonerNames}'
-)
 
-print(len(summonerNames))
-browser.close()
+isLiveGame = True
+
+if isLiveGame:
+
+	live_game = bsoup(requests.get('https://u.gg/lol/profile/br1/vanshader/live-game').text, features='html.parser').prettify()
+	open('live_game.html', 'w', encoding='utf-8').write(str(live_game))
+	
+	print(live_game.find_all(
+		'div',
+		class_ = 'player-name',
+		limit = 10
+
+	))
+################################################# riot api
+#driver_webscrapping()
+
